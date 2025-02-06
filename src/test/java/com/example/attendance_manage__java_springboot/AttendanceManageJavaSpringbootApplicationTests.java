be5bf0fc1dc698ff38cd73e466539ba79d8a06cb
Lisa-Shima <shimalisa2@gmail.com>
2025-02-06 18:59:21 +0200

    Committing - Connection to db

---
+++ src/test/java/com/example/attendance_manage__java_springboot/AttendanceManageJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attendance_manage__java_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManageJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
